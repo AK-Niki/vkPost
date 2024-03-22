adaf1556a7a3d3a0dd78d43b733183f0b291c165
Tasy <test@neto.com>
2024-03-22 18:58:57 +0300

    JUnit

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -50,4 +50,40 @@ class WallServiceTest {
 
         assertFalse(result)
     }
+
+    @Test(expected = PostNotFoundException::class)
+    fun shouldThrow() {
+        val comment = Comment(
+            id = 1,
+            postId = 100,
+            fromId = 123,
+            text = "Test comment"
+        )
+        WallService.createComment(comment.postId, comment)
+    }
+
+    @Test
+    fun shouldAddCommentToExistingPost() {
+        // Arrange
+        val post = Post(
+            id = 1,
+            ownerId = 123,
+            fromId = 456,
+            date = 18022024,
+            text = "Пост намбер уан!"
+        )
+        WallService.add(post)
+
+        val comment = Comment(
+            id = 1,
+            postId = 1,
+            fromId = 789,
+            text = "Комментарий к посту"
+        )
+
+        val addedComment = WallService.createComment(comment.postId ?: 0, comment)
+
+        assertEquals(comment, addedComment)
+    }
+
 }
